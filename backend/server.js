2d69bdb5913d0072a4b261cc6dde6b363c95b769
SaqJun <bdhdvzg@gmail.com>
2026-02-09 22:07:21 +0530

    Adding encryption to backend data

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000
 
 // Middleware
 app.use(cors({
-    origin: ["http://localhost:5173", `${process.env.CLIENT_URI}`, "https://trixon-three.vercel.app"],
+    origin: ["http://localhost:5173", `${process.env.CLIENT_URI}`, "https://trixon-three.vercel.app", "https://www.trixon.cloud", "https://trixon.cloud"],
     methods: ["POST", "GET", "PUT", "DELETE"],
     credentials: true
 }))
