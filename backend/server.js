49d08c053068099c3d7e78a9cc865f669f003bf6
SaqJun <bdhdvzg@gmail.com>
2026-02-09 20:56:30 +0530

    Fixing .env calls

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000
 
 // Middleware
 app.use(cors({
-    origin: ["http://localhost:5173", `${process.env.CLIENT_URI}`],
+    origin: ["http://localhost:5173", `${process.env.CLIENT_URI}`, "https://trixon-three.vercel.app"],
     methods: ["POST", "GET", "PUT", "DELETE"],
     credentials: true
 }))
